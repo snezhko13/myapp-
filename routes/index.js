dc1fd983eba477a182fa0ab570dfc939f6341ca2
snezhko13 <snezhkoandrew1999@gmail.com>
2018-12-11 17:00:17 +0200

    initial2

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var connection = require('../connection');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 /*	connection.query('Select * FROM tshirts;', function(error,results,fields){*/
-	connection.query('Select * FROM tshirts; Select * FROM hoodies', [1, 2], function(error,results,fields){
+	connection.query('Select * FROM tshirts; Select * FROM hoodies; Select * FROM sweatshirts', [1, 2, 3], function(error,results,fields){
 		console.log(results);
 		if(error) throw error
 		res.render('index',{
